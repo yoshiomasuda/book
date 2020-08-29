c10976047e3a08f16149423c9034dd3ba6663baa
vagrant <vagrant@homestead>
2020-08-29 02:16:29 +0000

    20200829

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    book
                 </div>
 
                 <div class="links">
